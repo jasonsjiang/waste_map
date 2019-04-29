4d3fb231fcde15f52d7b7342cf1ea58f7383795d
Jason Jiang <jasonsunjiang@gmail.com>
2019-04-29 11:36:22 -0700

    Updates

---
+++ precache-manifest.270277d4e5816e900296140fffc21247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd544683acd3336e3c2bfeab713be18",
+    "url": "/waste_map/index.html"
+  },
+  {
+    "revision": "4b7182c919e578681367",
+    "url": "/waste_map/static/css/main.3000c824.chunk.css"
+  },
+  {
+    "revision": "02d295af505587052d7e",
+    "url": "/waste_map/static/js/2.e0a47b73.chunk.js"
+  },
+  {
+    "revision": "4b7182c919e578681367",
+    "url": "/waste_map/static/js/main.4e32000c.chunk.js"
+  },
+  {
+    "revision": "ed735a12a3bf1d91e83b",
+    "url": "/waste_map/static/js/runtime~main.6140a682.js"
+  },
+  {
+    "revision": "bf9d5028c505bd141b1c2e7a67dde114",
+    "url": "/waste_map/static/media/cmunrm-webfont.bf9d5028.woff"
+  }
+]);
